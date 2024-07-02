d36190203951d76640a86d589dbbb154f217863e
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2024-07-02 12:26:48 +0800

    inaccessible obhj

--- patchwork/common/utils/user_config.py
+++ patchwork/common/utils/user_config.py
@@ -10,7 +10,7 @@ from pydantic import BaseModel
 from patchwork.managed_files import CONFIG_FILE
 
 
-class UserConfig(BaseModel):
+class __UserConfig(BaseModel):
     id: str = hashlib.sha256(str(uuid.getnode()).encode()).hexdigest()
 
     def persist(self):
@@ -19,21 +19,14 @@ class UserConfig(BaseModel):
         except Exception as e:
             logging.error(f"Failed to persist user config: {e}")
 
-    @staticmethod
-    def read() -> 'UserConfig' | None:
-        try:
-            return UserConfig.model_validate_strings(CONFIG_FILE.read_text())
-        except Exception as e:
-            logging.error(f"Failed to read user config: {e}")
-            return None
-
 
 @functools.lru_cache(maxsize=None)
 def get_user_config():
-    user_config = UserConfig.read()
-    if user_config is not None:
-        return user_config
+    try:
+        return __UserConfig.model_validate_strings(CONFIG_FILE.read_text())
+    except Exception as e:
+        logging.error(f"Failed to read user config: {e}")
 
-    user_config = UserConfig()
+    user_config = __UserConfig()
     user_config.persist()
     return user_config
