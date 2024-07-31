42ce371d7130c0d15178ee28541ecbd043c1342b
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2024-07-31 13:25:17 +0800

    fix semgrep types

--- patchwork/steps/ScanSemgrep/typed.py
+++ patchwork/steps/ScanSemgrep/typed.py
@@ -4,8 +4,8 @@ from patchwork.common.utils.step_typing import StepTypeConfig
 
 
 class ScanSemgrepInputs(TypedDict, total=False):
-    sarif_file_path: Annotated[str, StepTypeConfig(is_config=True, is_path=True, or_op=["sarif_values"])]
-    sarif_values: Annotated[Dict, StepTypeConfig(or_op=["sarif_file_path"])]
+    sarif_file_path: Annotated[str, StepTypeConfig(is_config=True, is_path=True)]
+    sarif_values: Dict
     semgrep_extra_args: Annotated[str, StepTypeConfig(is_config=True)]
 
 
