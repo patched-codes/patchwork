8f11ee8268a299467d7caba499b569e51e476f11
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2024-08-13 00:37:57 -0700

    pretty up

--- patchwork/steps/SimplifiedLLMOnce/typed.py
+++ patchwork/steps/SimplifiedLLMOnce/typed.py
@@ -26,12 +26,12 @@ class SimplifiedLLMOnceInputs(__SimplifiedLLMOnceInputsRequired, total=False):
             is_config=True,
             or_op=["openai_api_key", "google_api_key", "anthropic_api_key"],
             msg=f"""\
-    Model API key not found.
-    Please login at: "{TOKEN_URL}"
-    Please go to the Integration's tab and generate an API key.
-    Please copy the access token that is generated, and add `--patched_api_key=<token>` to the command line.
-    
-    If you are using a OpenAI API Key, please set `--openai_api_key=<token>`.""",
+Model API key not found.
+Please login at: "{TOKEN_URL}"
+Please go to the Integration's tab and generate an API key.
+Please copy the access token that is generated, and add `--patched_api_key=<token>` to the command line.
+
+If you are using a OpenAI API Key, please set `--openai_api_key=<token>`.""",
         ),
     ]
     google_api_key: Annotated[
