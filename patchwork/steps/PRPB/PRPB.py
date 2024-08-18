1636887b5716633d3278b37d22568a0b51df372e
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2024-08-18 16:23:56 -0700

    add PRPB

---
+++ patchwork/steps/PRPB/PRPB.py
@@ -0,0 +1,30 @@
+from patchwork.common.utils.utils import exclude_none_dict
+from patchwork.step import Step
+from patchwork.steps import PR
+from patchwork.steps.PR.typed import PRInputs
+from patchwork.steps.PRPB.typed import PRPBInputs, PRPBOutputs
+
+
+class PRPB(Step, input_class=PRPBInputs, output_class=PRPBOutputs):
+
+    def __init__(self, inputs):
+        super().__init__(inputs)
+        key_map = dict(path=inputs['path_key'])
+        if inputs.get('title_key') is not None:
+            key_map['commit_message'] = inputs['title_key']
+        if inputs.get('message_key') is not None:
+            key_map['patch_message'] = inputs['message_key']
+
+        self.modified_files = []
+        for modified_file in inputs.get('modified_files', []):
+            converted_modified_file = {
+                key: modified_file.get(mapped_key) for key, mapped_key in key_map.items()
+            }
+            self.modified_files.append(converted_modified_file)
+        self.inputs = inputs
+
+    def run(self):
+        pr = PR({**self.inputs, 'modified_code_files': self.modified_files})
+        pr_outputs = pr.run()
+
+        return pr_outputs
---
+++ patchwork/steps/PRPB/typed.py
@@ -0,0 +1,38 @@
+from typing_extensions import Annotated, List, TypedDict, Dict
+
+from patchwork.common.utils.step_typing import StepTypeConfig
+
+
+class __PRPBInputsRequired(TypedDict):
+    # CommitChangesInputs & PreparePRInputs
+    modified_files: List[Dict]
+    path_key: str
+
+
+class PRPBInputs(__PRPBInputsRequired, total=False):
+    title_key: str
+    message_key: str
+    # CommitChangesInputs
+    disable_branch: Annotated[bool, StepTypeConfig(is_config=True)]
+    force_branch_creation: Annotated[bool, StepTypeConfig(is_config=True)]
+    branch_prefix: Annotated[str, StepTypeConfig(is_config=True)]
+    branch_suffix: Annotated[str, StepTypeConfig(is_config=True)]
+    # PreparePRInputs
+    pr_header: Annotated[str, StepTypeConfig(is_config=True)]
+    # CreatePRInputs
+    pr_title: Annotated[str, StepTypeConfig(is_config=True)]
+    force_pr_creation: Annotated[bool, StepTypeConfig(is_config=True)]
+    disable_pr: Annotated[bool, StepTypeConfig(is_config=True)]
+    scm_url: Annotated[str, StepTypeConfig(is_config=True)]
+    gitlab_api_key: Annotated[str, StepTypeConfig(is_config=True)]
+    github_api_key: Annotated[str, StepTypeConfig(is_config=True)]
+
+
+class PRPBOutputs(TypedDict):
+    # CommitChangesOutputs
+    base_branch: str
+    target_branch: str
+    # PreparePROutputs
+    pr_body: str
+    # CreatePROutputs
+    pr_url: str
