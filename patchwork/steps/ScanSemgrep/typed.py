c666d58c11ed5b6e8b3fbc4d8de4203f77a70e56
TY <42710806+CTY-git@users.noreply.github.com>
2024-10-15 19:10:00 +0800

    Add paths to inputs of ScanSemgrep step (#952)
    * add the input
    
    * fix
    
    * bump version and fix split
    
    * add path key

--- patchwork/steps/ScanSemgrep/typed.py
+++ patchwork/steps/ScanSemgrep/typed.py
@@ -7,6 +7,8 @@ class ScanSemgrepInputs(TypedDict, total=False):
     sarif_file_path: Annotated[str, StepTypeConfig(is_config=True, is_path=True)]
     sarif_values: str
     semgrep_extra_args: Annotated[str, StepTypeConfig(is_config=True)]
+    paths: str
+    path_key: str
 
 
 class ScanSemgrepOutputs(TypedDict):
