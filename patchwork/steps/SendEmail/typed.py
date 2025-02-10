180078bd69c801bb8669cfc420c31cea53115266
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2025-02-10 16:28:18 +0800

    Add ability to send email as a step

---
+++ patchwork/steps/SendEmail/typed.py
@@ -0,0 +1,20 @@
+from typing_extensions import Annotated, Dict, TypedDict, Any
+
+from patchwork.common.utils.step_typing import StepTypeConfig
+
+
+class __SendEmailRequiredInputs(TypedDict):
+    sender_email: str
+    recipient_email: str
+    sender_email_password: str
+
+
+class SendEmailInputs(__SendEmailRequiredInputs, total=False):
+    email_template_value: dict[str, Any]
+    subject: str
+    body: str
+    smtp_host: str
+    smtp_port: int
+
+class SendEmailOutputs(TypedDict):
+    is_slack_message_sent: bool
