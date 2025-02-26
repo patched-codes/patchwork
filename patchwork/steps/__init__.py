d67c6567c96e1cfd30ce9d7845215eaae549d91f
Arpit Roopchandani <17565234+whoisarpit@users.noreply.github.com>
2025-02-26 18:37:41 +0800

    Add BrowserUse step

--- patchwork/steps/__init__.py
+++ patchwork/steps/__init__.py
@@ -53,6 +53,7 @@ from patchwork.steps.SendEmail.SendEmail import SendEmail
 from patchwork.steps.SimplifiedLLM.SimplifiedLLM import SimplifiedLLM
 from patchwork.steps.SimplifiedLLMOnce.SimplifiedLLMOnce import SimplifiedLLMOnce
 from patchwork.steps.SlackMessage.SlackMessage import SlackMessage
+from patchwork.steps.BrowserUse.BrowserUse import BrowserUse
 
 # Compatibility Aliases
 JoinListPB = JoinList
@@ -112,4 +113,5 @@ __all__ = [
     "JoinList",
     "JoinListPB",
     "GetTypescriptTypeInfo",
+    "BrowserUse",
 ]
