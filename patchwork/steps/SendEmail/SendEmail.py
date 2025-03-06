dc95064cafb1b77aee83a3b8b9b2ab8c8c97b9d5
TY <42710806+CTY-git@users.noreply.github.com>
2025-03-06 17:16:16 +0800

    Multiple recipient email (#1418)
    * add multiple recipient parsing to email
    
    * bump version
    
    * fix to part
    
    * use easier format

--- patchwork/steps/SendEmail/SendEmail.py
+++ patchwork/steps/SendEmail/SendEmail.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import smtplib
-from email.mime.text import MIMEText
+from email.message import EmailMessage
 
+from patchwork.common.utils.input_parsing import parse_to_list
 from patchwork.common.utils.utils import mustache_render
 from patchwork.step import Step
 from patchwork.steps.SendEmail.typed import SendEmailInputs, SendEmailOutputs
@@ -15,7 +16,7 @@ class SendEmail(Step, input_class=SendEmailInputs, output_class=SendEmailOutputs
         self.subject = inputs.get("subject", "Patchwork Execution Email")
         self.body = inputs.get("body", "Patchwork Execution Email")
         self.sender_email = inputs["sender_email"]
-        self.recipient_email = inputs["recipient_email"]
+        self.recipient_email = parse_to_list(inputs["recipient_email"], [" ", ","])
         self.smtp_host = inputs.get("smtp_host", "smtp.gmail.com")
         self.smtp_username = inputs["smtp_username"]
         self.smtp_password = inputs["smtp_password"]
@@ -24,10 +25,11 @@ class SendEmail(Step, input_class=SendEmailInputs, output_class=SendEmailOutputs
         self.is_ssl = bool(inputs.get("is_smtp_ssl", False))
 
     def run(self) -> dict:
-        msg = MIMEText(mustache_render(self.body, self.email_template_value))
+        msg = EmailMessage()
+        msg.set_content(mustache_render(self.body, self.email_template_value))
         msg["Subject"] = mustache_render(self.subject, self.email_template_value)
         msg["From"] = self.sender_email
-        msg["To"] = self.recipient_email
+        msg["To"] = ", ".join(self.recipient_email)
         if self.reply_message_id is not None:
             msg.add_header("Reference", self.reply_message_id)
             msg.add_header("In-Reply-To", self.reply_message_id)
@@ -38,6 +40,5 @@ class SendEmail(Step, input_class=SendEmailInputs, output_class=SendEmailOutputs
 
         with smtp_clazz(host=self.smtp_host, port=self.smtp_port) as mailserver:
             mailserver.login(self.smtp_username, self.smtp_password)
-            mailserver.sendmail(self.sender_email, self.recipient_email, msg.as_string())
-
+            mailserver.send_message(msg)
         return dict()
