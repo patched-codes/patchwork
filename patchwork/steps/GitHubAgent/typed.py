a4678c79a57baffd1ccebe3f683f6949d2f79677
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2025-03-06 11:29:01 +0800

    add mustache render to gh-agent

--- patchwork/steps/GitHubAgent/typed.py
+++ patchwork/steps/GitHubAgent/typed.py
@@ -6,8 +6,7 @@ from patchwork.common.utils.step_typing import StepTypeConfig
 class GitHubAgentInputs(TypedDict, total=False):
     base_path: str
     prompt_value: Dict[str, Any]
-    system_prompt: str
-    user_prompt: str
+    task: str
     max_llm_calls: Annotated[int, StepTypeConfig(is_config=True)]
     openai_api_key: Annotated[
         str, StepTypeConfig(is_config=True, or_op=["patched_api_key", "google_api_key", "anthropic_api_key"])
