ccb55323ecc6b857e438bf26287cc4d142c813b8
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2025-03-06 11:31:05 +0800

    better typing

--- patchwork/steps/GitHubAgent/typed.py
+++ patchwork/steps/GitHubAgent/typed.py
@@ -3,10 +3,12 @@ from typing_extensions import Annotated, Any, Dict, List, TypedDict
 from patchwork.common.utils.step_typing import StepTypeConfig
 
 
-class GitHubAgentInputs(TypedDict, total=False):
+class __GitHubAgentRequiredInputs(TypedDict):
+    task: str
+
+class GitHubAgentInputs(__GitHubAgentRequiredInputs, total=False):
     base_path: str
     prompt_value: Dict[str, Any]
-    task: str
     max_llm_calls: Annotated[int, StepTypeConfig(is_config=True)]
     openai_api_key: Annotated[
         str, StepTypeConfig(is_config=True, or_op=["patched_api_key", "google_api_key", "anthropic_api_key"])
