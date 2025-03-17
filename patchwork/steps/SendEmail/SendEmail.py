6a72f9b8b608e2a0ac5d083d168480aa428970d4
TY <42710806+CTY-git@users.noreply.github.com>
2025-03-17 12:06:02 +0800

    Fix single email (#1495)
    * fix
    
    * add semgrep ignore

--- patchwork/steps/SendEmail/SendEmail.py
+++ patchwork/steps/SendEmail/SendEmail.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import smtplib
 from email.message import EmailMessage
 
-from patchwork.common.utils.input_parsing import parse_to_list
 from patchwork.common.utils.utils import mustache_render
 from patchwork.step import Step
 from patchwork.steps.SendEmail.typed import SendEmailInputs, SendEmailOutputs
@@ -16,7 +15,7 @@ class SendEmail(Step, input_class=SendEmailInputs, output_class=SendEmailOutputs
         self.subject = inputs.get("subject", "Patchwork Execution Email")
         self.body = inputs.get("body", "Patchwork Execution Email")
         self.sender_email = inputs["sender_email"]
-        self.recipient_email = parse_to_list(inputs["recipient_email"], [" ", ","])
+        self.recipient_email = inputs["recipient_email"]
         self.smtp_host = inputs.get("smtp_host", "smtp.gmail.com")
         self.smtp_username = inputs["smtp_username"]
         self.smtp_password = inputs["smtp_password"]
@@ -29,7 +28,7 @@ class SendEmail(Step, input_class=SendEmailInputs, output_class=SendEmailOutputs
         msg.set_content(mustache_render(self.body, self.email_template_value))
         msg["Subject"] = mustache_render(self.subject, self.email_template_value)
         msg["From"] = self.sender_email
-        msg["To"] = ", ".join(self.recipient_email)
+        msg["To"] = self.recipient_email
         if self.reply_message_id is not None:
             msg.add_header("Reference", self.reply_message_id)
             msg.add_header("In-Reply-To", self.reply_message_id)
