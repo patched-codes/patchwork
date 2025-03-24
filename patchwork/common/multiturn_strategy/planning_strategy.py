df7129c581770b0b00aed0f63652496150c12e2a
TIANYOU CHEN <42710806+CTY-git@users.noreply.github.com>
2025-03-24 15:28:10 +0800

    add planner db agent to planner docstring

--- patchwork/common/multiturn_strategy/planning_strategy.py
+++ patchwork/common/multiturn_strategy/planning_strategy.py
@@ -86,6 +86,42 @@ class PlanningStrategy:
         executor_tool_set: dict[str, Tool],
         example_json: Union[str, dict[str, Any]] = '{"output":"output text"}',
     ):
+        """
+        Use this like this::
+
+            class DatabaseAgent(Step, input_class=DatabaseAgentInputs, output_class=DatabaseAgentOutputs):
+                 def __init__(self, inputs):
+                     super().__init__(inputs)
+
+                     llm_client = AioLlmClient.create_aio_client(inputs)
+
+                     data = inputs.get("prompt_value", {})
+                     self.task = mustache_render(inputs["task"], data)
+
+                     db_dialect = inputs["db_dialect"]
+                     self.planner = PlanningStrategy(
+                         llm_client,
+                         planner_system_prompt=f'''\\
+            You are a {db_dialect} database query planning assistant. You are tasked to plan the steps to assist with the provided task.
+            You will not execute the steps in the plan. The user will do that instead.
+            The first step of the plan should be as follows:
+            1. Tell me all tables currently available.
+                        After the list of table names is provided, get the DDL of the tables that is relevant.
+                        Your steps should be clear and concise like the following example:
+            1. Tell me the column descriptions of the table `orders`.
+            2. Execute the SQL Query: `SELECT * FROM orders`
+                        After every step, you will be asked to edit the plan so feel free to plan 1 step at a time.
+            ''',
+                        executor_system_prompt=f'''\\
+            You are a {db_dialect} database query execution assistant. You will be provided instructions on what to do.
+            ''',
+                    )
+
+                def run(self) -> dict:
+                    planner_response = self.planner.run(self.task, 10)
+                    return {**planner_response, **self.planner.usage()}
+
+        """
         self.planner = Agent(
             llm_client,
             name="Planner",
